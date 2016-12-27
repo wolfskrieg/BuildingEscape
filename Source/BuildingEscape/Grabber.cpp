750bc963303be3dc6cbb04881778ee9689e8701e
Wolfskrieg <milosdanilovic123@gmail.com>
2016-12-27 16:14:04 +0100

    Grab func in Grabber class start. Some videos passed.

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -16,16 +16,41 @@ UGrabber::UGrabber()
 
 
 // Called when the game starts
-void UGrabber::BeginPlay()
-{
+void UGrabber::BeginPlay() {
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty"))
 
-	// ...
+	// Look for attached physics handle
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+
+	if(PhysicsHandle) {
+	
+		
+	} else {
+	
+		UE_LOG(LogTemp, Error, TEXT("Object %s: Physics handle not found!"), *GetOwner()->GetName())
+	}
+
+	if(InputComponent) {
+
+		UE_LOG(LogTemp, Warning, TEXT("Object %s: Input component found!"), *GetOwner()->GetName())
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		UE_LOG(LogTemp, Error, TEXT("Object %s: Input component not found!"), *GetOwner()->GetName())
+
+	} else {
+
+		UE_LOG(LogTemp, Error, TEXT("Object %s: Input component not found!"), *GetOwner()->GetName())
+	}
 	
 }
 
+void UGrabber::Grab() {
+
+	UE_LOG(LogTemp, Warning, TEXT("Grab pressed"))
+
+}
 
 // Called every frame
 void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
@@ -33,5 +58,44 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
 	// ...
+	FVector out_location;
+	FRotator out_rotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(out_location, out_rotation);
+
+	//UE_LOG(LogTemp, Warning, TEXT("Location: %s , Rotation: %s"), *out_location.ToString(), *out_rotation.ToString());
+
+	float Reach = 100.f;
+	FVector LineTraceEnd = out_location + out_rotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		out_location,
+		LineTraceEnd,
+		FColor::Red,
+		false,
+		0.f,
+		0.f,
+		2.f
+
+	);
+	FHitResult Hit;
+	FCollisionQueryParams CollisionParams(FName(TEXT("")), false, GetOwner());
+
+	GetWorld()->LineTraceSingleByObjectType(
+	
+		Hit,
+		out_location,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		CollisionParams
+	
+	);
+
+	AActor* ActorHit = Hit.GetActor();
+	if(ActorHit) {
+	
+		//UE_LOG(LogTemp, Warning, TEXT("%s got hit!"), *(ActorHit->GetName()))
+	}
+	
 }
 
--- Source/BuildingEscape/Grabber.h
+++ Source/BuildingEscape/Grabber.h
@@ -15,12 +15,16 @@ public:
 	// Sets default values for this component's properties
 	UGrabber();
 
+	void Grab();
 	// Called when the game starts
 	virtual void BeginPlay() override;
 	
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
-		
+private:
+
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComponent = nullptr;
 	
 };
